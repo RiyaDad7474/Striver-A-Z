13ff1b14d625fde2bba1d384fa641f09bbae522a
RIYA <riyadad7474@gmail.com>
2025-10-27 23:27:21 +0530

    BS

---
+++ Step 4 -Binary Search 1D 2D Arrays  Search Space/Lec - 1 BS on 1D Arrays/Find_the_first_or_last_occurrence_of_a_given_number_in_a_sorted_array.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurance(vector<int> arr, int n, int k)
+{
+  int low = 0, high = n - 1;
+  int first = -1;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == k)
+    {
+      first = mid;
+      high = mid - 1;
+    }
+    else if (arr[mid] < k)
+    {
+      low = mid + 1;
+    }
+    else
+      high = mid - 1;
+  }
+  return first;
+}
+int lastOccurance(vector<int> arr, int n, int k)
+{
+  int low = 0, high = n - 1;
+  int last = -1;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    // maybe an answer
+    if (arr[mid] == k)
+    {
+      last = mid;
+      low = mid + 1;
+    }
+    else if (arr[mid] < k)
+    {
+      low = mid + 1;
+    }
+    else
+      high = mid - 1;
+  }
+  return last;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+  int first = firstOccurance(arr, n, k);
+  if (first == -1)
+    return {-1, -1};
+  int last = lastOccurance(arr, n, k);
+  return {first, last};
+}
