9585dd3d144002c655965ca5fe5438f4af6c87ae
RIYA <riyadad7474@gmail.com>
2025-11-05 16:22:29 +0530

    done

--- Step 4 -Binary Search 1D 2D Arrays  Search Space/Lec - 1 BS on 1D Arrays/Find_peak_element.cpp
+++ Step 4 -Binary Search 1D 2D Arrays  Search Space/Lec - 1 BS on 1D Arrays/Find_peak_element.cpp
@@ -1,2 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int findPeakElement(vector<int> &nums)
+{
+  int n = nums.size();
+  if (n == 1)
+    return 0;
+  if (nums[0] > nums[1])
+    return 0;
+  if (nums[n - 1] > nums[n - 2])
+    return n - 1;
+
+  int low = 1, high = n - 2;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+    {
+      return mid;
+    }
+    else if (nums[mid] > nums[mid - 1])
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return -1;
+}
+
+int main()
+{
+  vector<int> nums = {1, 2, 3, 4, 5, 7, 6};
+  int result = findPeakElement(nums);
+  cout << "peak element index is " << result;
+  return 0;
+}
