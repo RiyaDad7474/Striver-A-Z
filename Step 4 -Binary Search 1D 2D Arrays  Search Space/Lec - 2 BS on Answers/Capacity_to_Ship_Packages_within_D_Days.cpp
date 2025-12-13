8109750f75bae54e5baa173c2632eee3021406f3
RIYA <riyadad7474@gmail.com>
2025-12-13 17:14:34 +0530

    done

---
+++ Step 4 -Binary Search 1D 2D Arrays  Search Space/Lec - 2 BS on Answers/Capacity_to_Ship_Packages_within_D_Days.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDays(vector<int> &weights, int cap)
+{
+  int d = 1, load = 0;
+  for (int i = 0; i < weights.size(); i++)
+  {
+    if (weights[i] + load > cap)
+    {
+      d++;
+      load = weights[i];
+    }
+    else
+    {
+      load += weights[i];
+    }
+  }
+  return d;
+}
+
+int shipWithinDays(vector<int> &weights, int days)
+{
+  int low = *max_element(weights.begin(), weights.end());
+  int high = accumulate(weights.begin(), weights.end(), 0);
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    int numberofDays = findDays(weights, mid);
+    if (numberofDays <= days)
+    {
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return low;
+}
+int main()
+{
+  vector<int> weights = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int days = 5;
+  int result = shipWithinDays(weights, days);
+  cout << result;
+  return 0;
+}
