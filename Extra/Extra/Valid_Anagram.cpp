1709ecf5b02a2b70cb0316931844eb29ef823dbf
RIYA <riyadad7474@gmail.com>
2025-12-22 15:46:34 +0530

    done

---
+++ Extra/Extra/Valid_Anagram.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t)
+{
+  sort(begin(s), end(s));
+  sort(begin(t), end(t));
+
+  return s == t;
+}
+
+int main()
+{
+  string s = "anagram", t = "nagaram";
+  cout << (isAnagram(s, t) ? "true" : "false") << endl;
+  return 0;
+}
--- Step 3- Solve Problems on Arrays/Lecture-2 Medium/tempCodeRunnerFile.cpp
+++ Step 3- Solve Problems on Arrays/Lecture-2 Medium/tempCodeRunnerFile.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long maxSubarraySum(int arr[], int n){
-    long long sum = 0, maxi = LONG_MIN, start, ansStart = -1,ansEnd = -1 ;
-    for(int i = 0;i<n;i++){
+long long maxSubarraySum(int arr[], int n)
+{
+    long long sum = 0, maxi = LONG_MIN, start, ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < n; i++)
+    {
 
-        if(sum == 0){
+        if (sum == 0)
+        {
             start = i;
         }
 
         sum += arr[i];
 
-        if(sum > maxi){
+        if (sum > maxi)
+        {
             maxi = sum;
-            ansStart = start , ansEnd = i;
+            ansStart = start, ansEnd = i;
         }
-
+    }
