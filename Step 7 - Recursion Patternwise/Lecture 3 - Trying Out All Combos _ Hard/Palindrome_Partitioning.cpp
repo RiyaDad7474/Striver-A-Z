b7610ffec111c3839e8565389909cfda90f04c0f
RIYA <riyadad7474@gmail.com>
2026-01-29 22:15:02 +0530

    palindrom partioning done

---
+++ Step 7 - Recursion Patternwise/Lecture 3 - Trying Out All Combos _ Hard/Palindrome_Partitioning.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// function declarations
+void func(int index, string &s, vector<string> &path, vector<vector<string>> &res);
+bool isPalindrome(string &s, int start, int end);
+
+vector<vector<string>> partition(string s)
+{
+  vector<vector<string>> res;
+  vector<string> path;
+  func(0, s, path, res);
+  return res;
+}
+
+void func(int index, string &s, vector<string> &path,
+          vector<vector<string>> &res)
+{
+  if (index == s.size())
+  {
+    res.push_back(path);
+    return;
+  }
+
+  for (int i = index; i < s.size(); i++)
+  {
+    if (isPalindrome(s, index, i))
+    {
+      path.push_back(s.substr(index, i - index + 1));
+      func(i + 1, s, path, res);
+      path.pop_back();
+    }
+  }
+}
+
+bool isPalindrome(string &s, int start, int end)
+{
+  while (start < end)
+  {
+    if (s[start++] != s[end--])
+      return false;
+  }
+  return true;
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  vector<vector<string>> result = partition(s);
+
+  for (auto &vec : result)
+  {
+    for (auto &str : vec)
+      cout << str << " ";
+    cout << endl;
+  }
+  return 0;
+}
