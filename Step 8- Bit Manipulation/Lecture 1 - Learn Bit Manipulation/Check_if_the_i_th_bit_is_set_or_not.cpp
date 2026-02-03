bf0d33550d39a1b561cd973b1169a3f70d8a9fd0
RIYA <riyadad7474@gmail.com>
2026-02-04 00:19:34 +0530

    Check_if_the_i_th_bit_is_set_or_not

--- Step 8- Bit Manipulation/Lecture 1 - Learn Bit Manipulation/Check_if_the_i_th_bit_is_set_or_not.cpp
+++ Step 8- Bit Manipulation/Lecture 1 - Learn Bit Manipulation/Check_if_the_i_th_bit_is_set_or_not.cpp
@@ -1,2 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int isIthBitSet(int n, int i)
+{
+  if (n & (1 << i))
+  {
+    return 1; // ith bit is set
+  }
+  else if ((n >> i) & 1 == 0)
+  {
+    return 0; // ith bit is not set
+  }
+  else
+  {
+    return 0; // ith bit is not set
+  }
+}
+
+int main()
+{
+  int n, i;
+  cin >> n >> i;
+  cout << isIthBitSet(n, i) << endl;
+  return 0;
+}
