a03a66d6bccae2e0a7e5819d2eefce7594e85197
RIYA <riyadad7474@gmail.com>
2026-02-05 17:20:53 +0530

    Remove the last set Bit

---
+++ Step 8- Bit Manipulation/Lecture 1 - Learn Bit Manipulation/Remove_the_last_set_Bit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeLastSetBit(int n)
+{
+  return (n & (n - 1));
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << removeLastSetBit(n) << endl;
+  return 0;
+}
