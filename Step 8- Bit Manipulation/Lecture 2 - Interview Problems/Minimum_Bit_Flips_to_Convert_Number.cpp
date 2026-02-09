34bbe5ba9a34892c2d55b02b48bc70654866e8ba
RIYA <riyadad7474@gmail.com>
2026-02-09 20:22:32 +0530

    Minimum Bit Flips to Convert Number

---
+++ Step 8- Bit Manipulation/Lecture 2 - Interview Problems/Minimum_Bit_Flips_to_Convert_Number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minBitFlips(int start, int goal)
+{
+  int ans = start ^ goal;
+  int cnt = 0;
+  for (int i = 0; i < 31; i++)
+  {
+    if (ans & (1 << i))
+      cnt = cnt + 1;
+  }
+  return cnt;
+}
+
+int main()
+{
+  int start, goal;
+  cin >> start >> goal;
+  cout << minBitFlips(start, goal);
+  return 0;
+}
