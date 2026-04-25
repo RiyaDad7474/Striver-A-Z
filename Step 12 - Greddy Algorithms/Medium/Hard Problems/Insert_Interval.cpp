93422a92fa59a9ff8d98310fed77c61fbaf6c721
RIYA <riyadad7474@gmail.com>
2026-04-25 18:06:47 +0530

    done

---
+++ Step 12 - Greddy Algorithms/Medium/Hard Problems/Insert_Interval.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// tc->O(N)
+// sc->O(N)
+vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+{
+  vector<vector<int>> result;
+  int n = intervals.size();
+  int i = 0;
+  while (i < n && intervals[i][1] < newInterval[0])
+  {
+    result.push_back(intervals[i]);
+    i++;
+  }
+  while (i < n && intervals[i][0] <= newInterval[1])
+  {
+    newInterval[0] = min(newInterval[0], intervals[i][0]);
+    newInterval[1] = max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  result.push_back(newInterval);
+  while (i < n)
+  {
+    result.push_back(intervals[i]);
+    i++;
+  }
+  return result;
+}
+
+int main()
+{
+  vector<vector<int>> intervals = {{1, 3}, {6, 9}};
+  vector<int> newInterval = {2, 5};
+
+  vector<vector<int>> ans = insert(intervals, newInterval);
+
+  for (auto x : ans)
+  {
+    cout << "[" << x[0] << "," << x[1] << "] ";
+  }
+  return 0;
+}
