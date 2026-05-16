4399c45adbc756888afd266c16eb4adc18423953
RIYA <riyadad7474@gmail.com>
2026-05-16 16:49:32 +0530

    done

---
+++ Step 13 - Binary Trees/Lecture 1 - Traversals/Inorder_Traversal_of_Binary_Tree.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+
+vector<int> ans;
+
+void inorder(Node *root)
+{
+  if (root == NULL)
+    return;
+
+  inorder(root->left);
+  ans.push_back(root->data);
+  inorder(root->right);
+}
+
+vector<int> inorderTraversal(Node *root)
+{
+  inorder(root);
+  return ans;
+}
+
+int main()
+{
+  Node *root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->right = new Node(4);
+
+  vector<int> result = inorderTraversal(root);
+
+  for (int i = 0; i < result.size(); i++)
+  {
+    cout << result[i] << " ";
+  }
+
+  return 0;
+}
